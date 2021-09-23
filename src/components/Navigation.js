f24b82aea427c7080b2f3ca9e6410f6644c481b8
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2021-09-23 11:40:05 +0200

    navlink & icons navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Navigation() {
   return (
@@ -8,6 +9,34 @@ export default function Navigation() {
           <img src='./assets/avatar.jpg' alt='background' />
           <h3> Edouard LARROCHE</h3>
         </div>
+      </div>
+      <div className='navigation'>
+        <ul>
+          <li>
+            <NavLink exact to='/' activeClassName='navActive'>
+              <i className='fas fa-home'></i>
+              <span>Acceuil</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to='skills' activeClassName='navActive'>
+              <i className='fas fa-mountain'></i>
+              <span>Skills</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to='portfolio' activeClassName='navActive'>
+              <i className='fas fa-images'></i>
+              <span>Portfolio</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to='contact' activeClassName='navActive'>
+              <i className='fas fa-address-book'></i>
+              <span>Contact</span>
+            </NavLink>
+          </li>
+        </ul>
       </div>
     </div>
   );
