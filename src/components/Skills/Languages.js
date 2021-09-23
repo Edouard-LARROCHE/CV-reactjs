9c502c80ecb542a8dc254dccfa3c0d117ce8bcd0
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2021-09-24 00:18:07 +0200

    add files skills

---
+++ src/components/Skills/Languages.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class Languages extends Component {
+  state = {
+    languages: [{}],
+  };
+  render() {
+    return <div></div>;
+  }
+}
---
+++ src/components/Skills/OthersSkills.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function OthersSkills() {
+  return <div>autres compétences</div>;
+}
