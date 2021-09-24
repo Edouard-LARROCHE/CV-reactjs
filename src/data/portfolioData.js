069982f856dd083d6316b5fa362dff741b0de353
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2021-09-24 15:31:47 +0200

    start page portfolio

---
+++ src/data/portfolioData.js
@@ -0,0 +1,10 @@
+export const portfolioData = [
+  {
+    id: 1,
+    name: 'Yoga with Joy',
+    languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
+    source: 'https://yoga-with-joy.netlify.app/',
+    info: 'Lorem tgrt gr yht hyt h th ththyuhj ujth yj tjt ry thu j bhtu j t uj yuj yujujyj yjy j yujiy ',
+    picture: './assets/joy.png',
+  },
+];
