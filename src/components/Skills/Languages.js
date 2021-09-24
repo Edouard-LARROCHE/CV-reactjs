d613312254ab92ec7abbf8e4105fc196156c7cc4
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2021-09-24 10:50:22 +0200

    class components languages

--- src/components/Skills/Languages.js
+++ src/components/Skills/Languages.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import ProgressBar from './ProgressBar';
 
 export default class Languages extends Component {
   state = {
-    languages: [{}],
+    languages: [
+      { id: 1, value: 'Javascript', xp: 0.6 },
+      { id: 2, value: 'Css', xp: 1 },
+      { id: 3, value: 'NodeJs', xp: 0.6 },
+      { id: 4, value: 'Typescript', xp: 0.2 },
+    ],
+    frameworks: [
+      { id: 1, value: 'ReactJs', xp: 0.6 },
+      { id: 2, value: 'Sass', xp: 0.3 },
+      { id: 3, value: 'Material UI', xp: 0.5 },
+      { id: 4, value: 'Styled Components', xp: 0.5 },
+    ],
   };
   render() {
-    return <div></div>;
+    let { languages, frameworks } = this.state;
+    return (
+      <div className='languagesFramworks'>
+        <ProgressBar languages={languages} className='languagesDisplay' title='Languages' />
+        <ProgressBar languages={frameworks} className='frameworksDisplay' title='Frameworks & Bibliothèques' />
+      </div>
+    );
   }
 }
--- src/components/Skills/OthersSkills.js
+++ src/components/Skills/OthersSkills.js
@@ -1,5 +1,47 @@
 import React from 'react';
 
 export default function OthersSkills() {
-  return <div>autres compétences</div>;
+  return (
+    <div className='otherSkills'>
+      <h3>Autres compétences</h3>
+      <div className='list'>
+        <ul>
+          <li>
+            <i className='fas fa-check-square' />
+            Méthodes agiles (SRUM)
+          </li>
+          <li>
+            <i className='fas fa-check-square' />
+            GitHub
+          </li>
+          <li>
+            <i className='fas fa-check-square' />
+            Firebase
+          </li>
+          <li>
+            <i className='fas fa-check-square' />
+            Figma
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <i className='fas fa-check-square' />
+            Autonomie
+          </li>
+          <li>
+            <i className='fas fa-check-square' />
+            Rigueur et précision
+          </li>
+          <li>
+            <i className='fas fa-check-square' />
+            Gestion d'équipe
+          </li>
+          <li>
+            <i className='fas fa-check-square' />
+            Prise d'initiative
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
