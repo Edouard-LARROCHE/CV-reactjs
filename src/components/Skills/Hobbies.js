b8ecd1dfadf61735864a1d5d1b8ea5c8ef37a5fe
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2021-09-24 12:35:55 +0200

    progress bar in skills

--- src/components/Skills/Hobbies.js
+++ src/components/Skills/Hobbies.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
 export default function hobbies() {
-  return <div>Loisirs</div>;
+  return (
+    <div className='hobbies'>
+      <h3>Intérêts</h3>
+      <ul>
+        <li className='hobbiy'>
+          <i className='fas fa-drum' />
+          <span>Batterie et percussion</span>
+        </li>
+        <li className='hobbiy'>
+          <i className='fas fa-laptop' />
+          <span>Nouvelle technologie</span>
+        </li>
+        <li className='hobbiy'>
+          <i className='fas fa-snowboarding' />
+          <span>Sport de glisse</span>
+        </li>
+        <li className='hobbiy'>
+          <i className='fas fa-video' />
+          <span>7éme art</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
