ad0c45f13af67cd91bc093557fd9240e5c61e163
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2021-10-12 10:16:14 +0200

    change picture profile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ export default function Navigation() {
     <div className='sidebar'>
       <div className='id'>
         <div className='idcontent'>
-          <img src='./assets/avatar.jpg' alt='background' />
+          <img src='./assets/avatar1.jpg' alt='background' />
           <h3> Edouard LARROCHE</h3>
         </div>
       </div>
