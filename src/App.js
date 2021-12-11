ab09c501640bc38a207557240db4b1ce228289e3
Edouard-LARROCHE <edouard-larroche_student2021@wilder.school>
2021-12-11 23:16:01 +0100

    add dark mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import useLocalStorage from 'use-local-storage';
+import Brightness4Icon from '@material-ui/icons/Brightness4';
+import Brightness5Icon from '@material-ui/icons/Brightness5';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
@@ -8,8 +11,18 @@ import Skills from './pages/Skills';
 import Admin from './pages/Admin';
 
 const App = () => {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
   return (
-    <div>
+    <div className='app' data-theme={theme}>
+      <div onClick={switchTheme}>
+        {theme === 'light' ? <Brightness4Icon style={{ cursor: 'pointer' }} /> : <Brightness5Icon style={{ cursor: 'pointer' }} />}
+      </div>
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Home} />
